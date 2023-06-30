77f1e5f0bc6f8e620f0f2fd76b42a8d771ef4232
hritikd3 <hritikdangi3@gmail.com>
2023-07-01 00:27:02 +0530

    update: .env and readme

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
 let dbConnection;
-const password = "q96qbuv61zwYiIyZ";
+const password = process.env.PASSWORD;
+// const password = "q96qbuv61zwYiIyZ";v
 let uri = `mongodb+srv://hritikd3:${password}@cluster1.nrli8e7.mongodb.net/?retryWrites=true&w=majority/test`;
 module.exports = {
   connectToDb: (cb) => {
